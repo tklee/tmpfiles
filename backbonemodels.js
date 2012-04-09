8954a1497205a26f4abf8708406f83d42d2dcf6c
ted <ted@ted-Lenovo-IdeaPad-Y550P.(none)>
2012-04-09 00:49:11 -0700

    none

--- backbonemodels.js
+++ backbonemodels.js
@@ -14,30 +14,30 @@
     }
   });
   
-  AppView = Backbone.View.extend({
-    el: $("body"),
-    initialize: function () {
-      this.friends = new Friends( null, { view: this });
-      //Create a friends collection when the view is initialized.
-      //Pass it a reference to this view to create a connection between the two
-    },
-    events: {
-      "click #add-friend":  "showPrompt",
-    },
-    showPrompt: function () {
-      var friend_name = prompt("Who is your friend?");
-      var friend_model = new Friend({ name: friend_name });
-      //Add a new friend model to our friend collection
-      this.friends.add( friend_model );
-    },
-    addFriendLi: function (model) {
-      //The parameter passed is a reference to the model that was added
-      $("#friends-list").append("<li>" + model.get('name') + "</li>");
-      //Use .get to receive attributes of the model
-    }
-  });
+//  AppView = Backbone.View.extend({
+//    el: $("body"),
+//    initialize: function () {
+//      this.friends = new Friends( null, { view: this });
+//      //Create a friends collection when the view is initialized.
+//      //Pass it a reference to this view to create a connection between the two
+//    },
+//    events: {
+//      "click #add-friend":  "showPrompt",
+//    },
+//    showPrompt: function () {
+//      var friend_name = prompt("Who is your friend?");
+//      var friend_model = new Friend({ name: friend_name });
+//      //Add a new friend model to our friend collection
+//      this.friends.add( friend_model );
+//    },
+//    addFriendLi: function (model) {
+//      //The parameter passed is a reference to the model that was added
+//      $("#friends-list").append("<li>" + model.get('name') + "</li>");
+//      //Use .get to receive attributes of the model
+//    }
+//  });
   
-  var appview = new AppView;
+//  var appview = new AppView;
 
 
 
@@ -61,13 +61,15 @@
 
      el: $("body"),
      initialize: function () {
+       this.friends = new Friends( null, { view: this });
 
        this.votes = new Votes( null, { view: this });
 
      },
 
-     events: {
-       "click #upvote-button": "showPrompt",
+     events: {    
+      "click #add-friend":  "showPrompt",
+      "click #upvote-button": "showPrompt",
      },
      
 
@@ -75,12 +77,23 @@
        var vote_model = new Vote({ upvotedby: 'user2' });
        this.votes.add( vote_model );
 
+       var friend_name = prompt("Who is your friend?");
+       var friend_model = new Friend({ name: friend_name });
+       //Add a new friend model to our friend collection
+       this.friends.add( friend_model );
      },
      increaseVotes: function (model) {
 
        $("#vote-count").append("<li>" + model.get('upvotedby') + "</li>");
        //possibly change the append to something like a "change" function
-     }
+     },
+
+
+    addFriendLi: function (model) {
+      //The parameter passed is a reference to the model that was added
+      $("#friends-list").append("<li>" + model.get('name') + "</li>");
+      //Use .get to receive attributes of the model
+    }
 
 
      //increaseVotes: function (collection) {
